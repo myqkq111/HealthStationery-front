1c2a31004c2e6eb28908f731022b0200ce9c75d5
명준 <LeeMaenggu9206@gmail.com>
2024-07-30 16:10:22 +0900

    no message

--- src/components/User/CustomModal.jsx
+++ src/components/User/CustomModal.jsx
@@ -1,9 +1,11 @@
+// 새 페이지가 뜨는게 아닌 기존페이지 위에 덧씌어지게 하는거임.
+
 import React from "react";
 import Modal from "react-modal";
 
 Modal.setAppElement("#root");
 
-const Modal = ({ isOpen, onClose, children }) => {
+const CustomModal = ({ isOpen, onClose, children }) => {
   return (
     <Modal
       isOpen={isOpen}
@@ -24,4 +26,4 @@ const Modal = ({ isOpen, onClose, children }) => {
   );
 };
 
-export default Modal;
+export default CustomModal;
