314a0cd00c76148757fa1e11b2f863afc274aa06
leeminwook1 <dlalsldnr2303@naver.com>
2024-07-31 15:18:43 +0900

    Feat: Shop 페이지 완성

--- src/components/MainPage/ShopPage/ProductItem.jsx
+++ src/components/MainPage/ShopPage/ProductItem.jsx
@@ -1,24 +1,50 @@
-import React from "react";
-import { useNavigate } from "react-router-dom"; // useNavigate로 변경
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-const ProductItem = ({ image, name, price, details, reviews, link }) => {
-  const navigate = useNavigate(); // useNavigate 사용
+const ProductItem = ({
+  image,
+  hoverImage,
+  name,
+  price,
+  details,
+  reviews,
+  link,
+}) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const navigate = useNavigate();
 
   // 카드 클릭 시 이동할 함수
   const handleClick = () => {
-    navigate(link); // navigate로 페이지 이동
+    navigate(link);
   };
 
   return (
     <div
-      className="border border-gray-300 p-4 rounded-lg shadow-md cursor-pointer"
+      className="p-1 cursor-pointer flex flex-col max-w-xs w-full h-auto"
       onClick={handleClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      <img src={image} alt={name} className="object-cover w-full h-40 mb-4" />
+      <div className="relative w-full h-64 mb-4">
+        <img
+          src={image}
+          alt={name}
+          className={`absolute inset-0 object-contain w-full h-full transition-opacity duration-300 ${
+            isHovered ? "opacity-0" : "opacity-100"
+          }`}
+        />
+        <img
+          src={hoverImage}
+          alt={`${name} hover`}
+          className={`absolute inset-0 object-contain w-full h-full transition-opacity duration-300 ${
+            isHovered ? "opacity-100" : "opacity-0"
+          }`}
+        />
+      </div>
       <h3 className="text-xl font-semibold mb-2">{name}</h3>
       <p className="text-lg text-gray-700 mb-2">{price}</p>
       <p className="text-md text-gray-600 mb-2">{details}</p>
-      <p className="text-sm text-red-600 mb-4">{reviews} 리뷰</p>
+      <p className="text-sm text-red-600 mb-4">리뷰: {reviews}</p>
     </div>
   );
 };
